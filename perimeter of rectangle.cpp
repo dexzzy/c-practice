ecd04f1befec79c9dfc684a829cecb5e08129d62
dexzzy <148374373+dexzzy@users.noreply.github.com>
2024-01-27 00:01:31 -0600

    Create perimeter of rectangle.cpp

---
+++ perimeter of rectangle.cpp
@@ -0,0 +1,28 @@
+// c++ practice
+// Perimeter of Rectangle
+#include <iostream>
+using namespace std;
+int main()
+{
+	// step 1 variables
+	int length1;
+	int width2;
+	int perimeter1;
+
+	//step 2 get input
+	cout << " Enter lenght1: ";
+	cin >> length1;
+	cout << "Enter width2: ";
+	cin >> width2;
+	// step 2 process input
+	perimeter1 = length1 * 2 + width2 * 2;
+
+	// display output
+	cout << " The perimeter is: " << perimeter1 << endl;
+
+
+
+
+	system("pause");
+	return 0;
+}
