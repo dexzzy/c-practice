5469e3480b975d08c184aa434836d04277cc2f98
dexzzy <148374373+dexzzy@users.noreply.github.com>
2024-01-26 23:57:37 -0600

    Create string practice.cpp

---
+++ string practice.cpp
@@ -0,0 +1,18 @@
+//c++ practice
+// String Practice
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string name;
+	string text = "Goodbye!";
+	cout << " Enter Your name: ";
+	cin >> name;
+	cout << "$$$$$$$$$$$$$$$$$$$" << endl;
+	cout << "Hello " << name << "!!!!" << endl;
+	cout << text << endl;
+	cout << "$$$$$$$$$$$$$$$$$$$" << endl;
+	system("pause");
+	return 0;
+}
